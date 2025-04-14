f30dfda30a6e4fd0eed535e9bf101cd74333315f
junaidiqbal-hashmi <junaidiqbalhashmi@gmail.com>
2025-04-14 15:54:46 +0500

    added a new column: category

--- models.py
+++ models.py
@@ -6,5 +6,7 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
+    description = Column(String, nullable=True) #new
     price = Column(Float)
     is_offer = Column(Boolean, default=False)
+    category = Column(String) #new
